104f23bb07e3aa73ce9e8d7ff0616bd2cc628ed4
quangloc.nguyen <you@example.com>
2020-10-26 09:20:33 +0700

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mosh';
+  courses = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' },
+  ];
+
+  
 }
