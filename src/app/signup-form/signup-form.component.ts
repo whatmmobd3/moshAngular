d36f3d3189486424883ee679f6cfc9936202be30
quangloc.nguyen <you@example.com>
2020-10-27 14:40:33 +0700

    specific validation

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { FormGroup, FormControl,Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'signup-form',
@@ -9,12 +9,11 @@ import { FormGroup, FormControl,Validators } from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('',Validators.required),
-    password: new FormControl('',Validators.required),
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', Validators.required),
   });
 
-  get username(){
-    return this.form.get('username')
+  get username() {
+    return this.form.get('username');
   }
-
 }
