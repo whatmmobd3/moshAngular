4e27887e02f82473796ba16c177d0fcaa82403d0
quangloc.nguyen <you@example.com>
2020-10-27 17:11:00 +0700

    update

--- src/app/common/validators/username.validators.ts
+++ src/app/common/validators/username.validators.ts
@@ -1,9 +1,15 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class UsernameValidator {
-  static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
-    if ((control.value as string).indexOf(' ') > 0)
+  static cannotContainSpace(e: AbstractControl): ValidationErrors | null {
+    if ((e.value as string).indexOf(' ') > 0)
       return { cannotContainSpace: true };
+    return null;
+  }
+
+  static shouldBeUnique(e: AbstractControl): ValidationErrors | null {
+    if (e.value === 'mosh') return { shouldBeUnique: true };
+
     return null;
   }
 }
