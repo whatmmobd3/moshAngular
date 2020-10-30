adc34039b98acd2b8ce5093bfab74eb69c78a7f6
quangloc.nguyen <you@example.com>
2020-10-30 10:08:04 +0700

    form Array

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
   templateUrl: './new-course-form.component.html',
   styleUrls: ['./new-course-form.component.css'],
 })
-export class NewCourseFormComponent implements OnInit {
-  constructor() {}
+export class NewCourseFormComponent {
+  form = new FormGroup({
+    topics: new FormArray([])
+  })
 
-  ngOnInit(): void {}
+  get topics() {
+    return this.form.get('topics') as FormArray
+  }
+
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value))
+
+  }
+
+  removeTopics(c: FormControl) {
+    let index = this.topics.controls.indexOf(c)
+    this.topics.removeAt(index)
+  }
+
+
 }
