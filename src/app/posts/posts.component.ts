7cf0414a7c5d23ce031f340dcb0c8d2134808c53
quangloc.nguyen <you@example.com>
2020-11-02 17:22:38 +0700

    onInnit

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,42 +1,26 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts: any;
   private url = 'https://jsonplaceholder.typicode.com/posts'
 
   constructor(private http: HttpClient) {
-    http.get(this.url).subscribe(res => {
-      this.posts = res
-    })
+
   }
 
-  createPost(input: HTMLInputElement) {
-    let post = { title: input.value }
-    input.value = ''
-    this.http.post(this.url, post).subscribe(res => {
-      // post['id'] = res.id
-      this.posts.splice(0, 0, post)
-    })
-  }
-
-  updatePost(post) {
-    this.http.patch(this.url + "/" + post.id, JSON.stringify({ isRead: true }))
-      .subscribe(res => {
-        console.log(res);
+  ngOnInit() {
+    this.http.get(this.url).
+      subscribe(res => {
+        this.posts = res
       })
   }
 
-  deletePost(post){
-    this.http.delete(this.url + "/" + post.id).subscribe(res =>{
-      let index = this.posts.indexOf(post)
-      this.posts.splice(index,1)
-    })
-  }
+
 
 }
